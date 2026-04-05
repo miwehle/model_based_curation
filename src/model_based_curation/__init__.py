501a2756a065e3421528ab6b41e860df86104b36
Michael Wehle <michael.wehle@mt-ag.com>
2026-04-06 01:54:34 +0200

    Rename loss bucket module

--- src/model_based_curation/__init__.py
+++ src/model_based_curation/__init__.py
@@ -1,6 +1,6 @@
 """Oeffentliche Paketoberflaeche fuer model_based_curation."""
 
-from model_based_curation.loss_buckets_streaming import split_by_loss
+from model_based_curation.loss_buckets import split_by_loss
 from model_based_curation.seq2seq_loss_scorer import Seq2SeqLossScorer
 
 __all__ = ["Seq2SeqLossScorer", "split_by_loss"]
